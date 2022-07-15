a184bd01e8366f6ecfc73174fbb0e606fe799454
jtb00 <javarrisb@gmail.con>
2022-07-15 13:06:06 -0400

    all files

---
+++ src/main/java/com/technicalnews/TechnicalNewsJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.technicalnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalNewsJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalNewsJavaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technicalnews/TechnicalNewsJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technicalnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalNewsJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
